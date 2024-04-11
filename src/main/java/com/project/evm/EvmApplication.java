4e007cbc92e775f720075f49da415c73233698e8
core <core@pop-os.localdomain>
2024-04-11 16:42:33 +0530

    first commit

---
+++ src/main/java/com/project/evm/EvmApplication.java
@@ -0,0 +1,13 @@
+package com.project.evm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvmApplication.class, args);
+	}
+
+}
