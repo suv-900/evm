6a44348bb9ff250c49286cc06e0681e7ddb9586a
core <core@pop-os.localdomain>
2024-04-11 17:50:42 +0530

    update models

--- src/main/java/com/project/evm/models/Admin.java
+++ src/main/java/com/project/evm/models/Admin.java
@@ -1,21 +1,30 @@
 package com.project.evm.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@Table(name="admin")
 public class Admin {
     @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private int id;
 
+    @Column(name="name",nullable=false,unique=true)
     private String name;
 
+    @Column(name="email",nullable=false)
     private String email;
 
+    @Column(name="password",nullable=false)
     private String password;
 
     public Admin(){}
--- src/main/java/com/project/evm/models/Host.java
+++ src/main/java/com/project/evm/models/Host.java
@@ -2,30 +2,43 @@ package com.project.evm.models;
 
 import java.util.List;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@Table(name="host")
 public class Host {
     
     @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private long id;
     
+    @Column(name="name",nullable=false)
     private String name;
     
+    @Column(name="email",nullable=false)
     private String email;
-   
+    
+    @Column(name="password",nullable=false)
     private String password;
     
+    @Column(name="phone",nullable=true)
     private String phone;
     
+    @Column(name="description",nullable=true)
     private String description;
     
+    @Column(name="events",nullable=true)
     private List<Event> events;
+    
     public Host(){}
 
     @Override
