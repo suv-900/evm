8e5ac6d501492f9bde318d0d2bc27647d79fa8bf
core <core@pop-os.localdomain>
2024-04-15 13:48:51 +0530

    annotation based error handling and content delivery

--- src/main/java/com/project/evm/controllers/UserController.java
+++ src/main/java/com/project/evm/controllers/UserController.java
@@ -6,35 +6,37 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.exceptions.TokenExpiredException;
-import com.google.gson.Gson;
-import com.project.evm.models.User;
+import com.project.evm.exceptions.UnauthorizedUserException;
+import com.project.evm.exceptions.UserNotFoundException;
+import com.project.evm.models.UserEntity;
 import com.project.evm.models.UserLogin;
 import com.project.evm.services.TokenService;
 import com.project.evm.services.UserService;
 
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 
 
 @RequestMapping("/users")
-@Controller
+@RestController
 public class UserController {
     private final static Logger log = LoggerFactory.getLogger(UserController.class);
 
@@ -44,24 +46,32 @@ public class UserController {
     @Autowired
     private TokenService tokenService;
 
+    @ResponseStatus(HttpStatus.OK)
     @PostMapping("/create")
-    public ResponseEntity<String> createUser(@Valid @RequestBody User user){
-        try{
-            //Generic Ex
-            userService.addUser(user);
+    public void createUser(@Valid @RequestBody UserEntity user,HttpServletResponse response)
+        throws JWTCreationException,Exception{
+        
+        //Generic Ex
+        userService.addUser(user);
 
-            //JWTCreationEx,IllegalArgEx
-            String token = tokenService.generateToken(user.getName());
+        //JWTCreationEx,IllegalArgEx
+        String token = tokenService.generateToken(user.getName());
 
-            HttpHeaders headers = new HttpHeaders();
-            headers.add("Token",token);
+        response.addHeader("Token",token);
+    }
 
-            return new ResponseEntity<>(headers,HttpStatus.OK);
-        }catch(Exception e){
-            log.info(e.getMessage());
-            e.printStackTrace();
-            return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public void handleException(Exception e){
+        log.error(e.getMessage());
+        e.printStackTrace();
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(JWTCreationException.class)
+    public void handleJWTCreationException(JWTCreationException e){
+        log.error(e.getMessage());
+        e.printStackTrace();
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -80,52 +90,58 @@ public class UserController {
         return errors;
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @PostMapping("/login")
-    public ResponseEntity<String> loginUser(@Valid @RequestBody UserLogin user){
-        try{
-            //IllegalArgsEx , GenericEx
-            boolean isOK = userService.loginUser(user);
+    public void loginUser(@Valid @RequestBody UserLogin user,HttpServletResponse response)throws Exception,UnauthorizedUserException{
+        //IllegalArgsEx , GenericEx
+        boolean isOK = userService.loginUser(user);
 
-            if(!isOK){
-                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-            }
-
-            String token = tokenService.generateToken(user.getName());
-
-            HttpHeaders headers = new HttpHeaders();
-            headers.add("Token",token);
-
-            return new ResponseEntity<>(headers,HttpStatus.OK);
-        }catch(Exception e){
-            log.info(e.getMessage());
-            e.printStackTrace();
-            return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+        if(!isOK){
+            throw new UnauthorizedUserException("Credentials dont match.");
         }
+
+        String token = tokenService.generateToken(user.getName());
+
+        response.addHeader("Token",token);
     }
 
-    @PostMapping("/update")
-    public ResponseEntity<String> updateUser(@RequestHeader(value = "Token",required = true) String token,
-            @RequestBody User userUpdates){
-        try{
-            String username = tokenService.extractUsername(token);
-            
-            userUpdates.setName(username);
-            
-            userService.updateUser(userUpdates);
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(UnauthorizedUserException.class)
+    public String handleUnauthorizedUserException(UnauthorizedUserException e){
+        log.error(e.getMessage());
+        return e.getMessage();
+    }
 
-            return new ResponseEntity<>(HttpStatus.OK);
-        }catch(TokenExpiredException e){
-            log.error(e.getMessage());
-            return new ResponseEntity<>("Token expired.",HttpStatus.UNAUTHORIZED);
+
+    // private String verifyTokenAndGetUsername(String token)throws TokenExpiredException,JWTVerificationException{
+    //     return tokenService.extractUsername(token);
+    // }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping("/update")
+    public void updateUser(@RequestHeader(value = "Token",required = true) String token,
+            @RequestBody UserEntity userUpdates)throws Exception,TokenExpiredException,JWTVerificationException{
         
-        }catch(JWTVerificationException e) {
-            log.error(e.getMessage());
-            return new ResponseEntity<>("Token malformed.",HttpStatus.BAD_REQUEST); 
-        
-        }catch(Exception e){
-            log.error(e.getMessage());
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        } 
+        String username = tokenService.extractUsername(token);
+
+        userUpdates.setName(username);
+            
+        userService.updateUser(userUpdates);
+    }
+
+   @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(JWTVerificationException.class)
+    public String handleJWTVerificationException(JWTVerificationException e){
+        log.error(e.getMessage());
+        return "Token malformed";
+    }
+
+
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(TokenExpiredException.class)
+    public String handleTokenExpiredException(TokenExpiredException e){
+        log.error(e.getMessage());
+        return "Token expired";
     }
 
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
@@ -134,13 +150,32 @@ public class UserController {
         return "Missing header: "+e.getHeaderName();
     }
 
+ 
+    @ResponseStatus(HttpStatus.FOUND)
     @GetMapping("/getUser/{id}")
-    public void getUser(){
+    public UserEntity getUser(@PathVariable("id")int id)throws Exception,UserNotFoundException{
+        UserEntity user = userService.getUser(id);
 
+        if( user == null ){
+            throw new UserNotFoundException();
+        }
+        return user;
     }
 
-    @DeleteMapping("/delete/{id}")
-    public void deleteUser(){
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(UserNotFoundException.class)
+    public String handleUserNotFoundException(UserNotFoundException e){
+        log.error(e.getMessage());
+        return e.getMessage();
+    }
 
+    @ResponseStatus(HttpStatus.OK)
+    @DeleteMapping("/delete")
+    public void deleteUser(@RequestHeader(value = "Token",required = true)String token)
+        throws TokenExpiredException,JWTVerificationException,Exception{
+
+        String username = tokenService.extractUsername(token);
+        
+        userService.deleteUserByUsername(username);
     }
 }
